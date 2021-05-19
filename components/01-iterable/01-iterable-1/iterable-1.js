bb9877692721e5ed3dd66ad49b065f8c1924d349
Darby Dixon III <ddixon@oberlin.edu>
2021-05-19 12:42:28 -0400

    Importing warm-up scripts.

--- components/01-iterable/01-iterable-1/iterable-1.js
+++
@@ -1,5 +0,0 @@
-(function () {
-  document.addEventListener('DOMContentLoaded', function () {
-    console.log("iterable-1 active");
-  });
-})();
--- components/example/example.js
+++
@@ -1,5 +0,0 @@
-(function () {
-  document.addEventListener('DOMContentLoaded', function () {
-    const variable = "this is a variable that I have changed again and again and again";
-  });
-})();
